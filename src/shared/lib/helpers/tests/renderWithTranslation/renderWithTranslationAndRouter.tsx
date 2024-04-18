6c94622773f70842fe91ca869c94d7f9ddd20e02
anastasiia_zam <anastasiya.zamashniuk@marketator.com>
2024-04-18 14:54:42 +0300

    change router in tests

--- src/shared/lib/helpers/tests/renderWithTranslation/renderWithTranslationAndRouter.tsx
+++ src/shared/lib/helpers/tests/renderWithTranslation/renderWithTranslationAndRouter.tsx
@@ -1,17 +1,15 @@
 import { render } from '@testing-library/react';
 import { ReactNode } from 'react';
 import { I18nextProvider } from 'react-i18next';
-import { BrowserRouter } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 import i18n from 'shared/config/i18n/i18nForTests';
 
 export function renderWithTranslationAndRouter(component: ReactNode) {
     return render(
-        <BrowserRouter>
-
+        <MemoryRouter>
             <I18nextProvider i18n={i18n}>
                 {component}
             </I18nextProvider>
-
-        </BrowserRouter>,
+        </MemoryRouter>,
     );
 }
